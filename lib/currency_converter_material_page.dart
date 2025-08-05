5b2f681b34bee661083e4cdccd5135e5b80411c2
PramudiWalakuluarachchi01 <pramudinethmini01@gmail.com>
2025-08-05 22:25:12 +0530

    `Refactor CurrencyConverterMaterialPage to use StatefulWidget and add conversion functionality`

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
 
-class CurrencyConverterMaterialPage extends StatelessWidget {
+class CurrencyConverterMaterialPage extends StatefulWidget {
   const CurrencyConverterMaterialPage({super.key});
+
   @override
-  Widget build(BuildContext context) {
+  State<CurrencyConverterMaterialPage> createState() =>
+      _CurrencyConverterMaterialPageState();
+}
+
+class _CurrencyConverterMaterialPageState
+    extends State<CurrencyConverterMaterialPage> {
+  double result = 0;
+  final TextEditingController textEditingController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) { 
     final border = OutlineInputBorder(
       borderRadius: BorderRadius.circular(5.0),
       borderSide: const BorderSide(
@@ -14,17 +25,31 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
     );
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 72, 87, 95),
+      appBar: AppBar(
+        title: const Text(
+          'Currency Converter',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: const Color.fromARGB(255, 72, 87, 95),
+        elevation: 0,
+        centerTitle: true,
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text(
-              'Currency Converter',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            Text(
+              result.toString(),
+              style: const TextStyle(
+                fontSize: 24,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
             ),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: TextField(
+                controller: textEditingController,
                 style: const TextStyle(fontSize: 18, color: Colors.black),
                 decoration: InputDecoration(
                   hintText: 'Enter amount in USD',
@@ -42,9 +67,43 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
                 ),
               ),
             ),
+
+            //button
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  result =
+                      double.parse(textEditingController.text) *
+                      81; // Example conversion rate
+                  build(context); // Rebuild to show the result
+                },
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  foregroundColor: Colors.white,
+                  minimumSize: const Size(double.infinity, 50),
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                  ),
+                ),
+                child: const Text('Convert'),
+              ),
+            ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class CurrencyConverterMaterialPagee extends StatelessWidget {
+  const CurrencyConverterMaterialPagee({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Currency Converter Material Pagee')),
+      body: const Center(child: Text('This is a placeholder page.')),
     );
   }
 }
